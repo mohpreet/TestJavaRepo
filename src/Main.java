76dc58a03a85957fbd9863d4a753d87f8c679f92
mohpreet <30866016+mohpreet@users.noreply.github.com>
2024-05-20 17:00:35 -0400

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,10 @@
 public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
+        // to see how IntelliJ IDEA suggests fixing it..... 
         System.out.printf("Hello and welcome!");
 
-        for (int itr = 1; itr <= 5; itr++) {
+        for (int itr = 1; itr <= 10; itr++) {
             System.out.println("This is itr = " + itr);
         }
     }
