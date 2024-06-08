35041893f7bc0b3e1ee1343acc0712c03f59efe8
MSD <mohpreet123@msd.com>
2024-06-08 17:15:26 -0400

    remove stub comments main.java

--- src/org/edu/msd/Main.java
+++ src/org/edu/msd/Main.java
@@ -1,10 +1,10 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 package org.edu.msd;
+
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it..... 
         System.out.printf("Hello and welcome!");
 
         for (int itr = 1; itr <= 110; itr++) {
