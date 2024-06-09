155b37a3129563fd5ebb26c4b14daf2b0e40014d
mohpreet <30866016+mohpreet@users.noreply.github.com>
2024-06-09 15:57:54 -0400

    feat(MSD-2022): update main,2nd time by remote

--- src/org/edu/msd/Main.java
+++ src/org/edu/msd/Main.java
@@ -9,11 +9,13 @@ package org.edu.msd;
 public class Main {
 
     public static void generateMessages(){
-        System.out.print("Hello and welcome!, Iterations Start");
+        System.out.print("Hello and welcome!, Iterations Started");
+        System.out.print("--------------------------------------");
         for (int itr = 1; itr <= 110; itr++) {
             System.out.println("This is itr = " + itr);
         }
-        System.out.println("This is done, Iterations End");
+        System.out.println("This is done, Iterations Ended");
+        System.out.print("--------------------------------------");
     }
     public static void main(String[] args) {
         generateMessages();
