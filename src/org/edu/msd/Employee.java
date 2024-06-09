98ab671e9a6e11fcc001da5c287a9e4fca87279c
MSD <mohpreet123@msd.com>
2024-06-09 14:07:07 -0400

    feat(MSD-12019) Create Employee Feature with following details:
    -Add getters and setter for employee
    -Add Default and parameterized constructors
    -Add stub for to test the employee class in tester
    -Create employee instance and test main
    -Overrdide to String in employee
    -Add print details method
    -Complete Test
    -Beautify main

--- src/org/edu/msd/Employee.java
+++ src/org/edu/msd/Employee.java
@@ -7,4 +7,64 @@ package org.edu.msd;
  * Date: 08-06-2024 05:59 pm
  */
 public class Employee {
+    private Integer employeeId;
+    private String employeeName;
+    private Integer departmentCode;
+    private Double salary;
+
+    public Integer getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(Integer employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public String getEmployeeName() {
+        return employeeName;
+    }
+
+    public void setEmployeeName(String employeeName) {
+        this.employeeName = employeeName;
+    }
+
+    public Integer getDepartmentCode() {
+        return departmentCode;
+    }
+
+    public void setDepartmentCode(Integer departmentCode) {
+        this.departmentCode = departmentCode;
+    }
+
+    public Double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Double salary) {
+        this.salary = salary;
+    }
+
+    public Employee(Integer employeeId, String employeeName, Integer departmentCode, Double salary) {
+        this.employeeId = employeeId;
+        this.employeeName = employeeName;
+        this.departmentCode = departmentCode;
+        this.salary = salary;
+    }
+
+    public Employee() {
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "employeeId=" + employeeId +
+                ", employeeName='" + employeeName + '\'' +
+                ", departmentCode=" + departmentCode +
+                ", salary=" + salary +
+                '}';
+    }
+
+    public void printDetails(){
+        System.out.println(this);
+    }
 }
